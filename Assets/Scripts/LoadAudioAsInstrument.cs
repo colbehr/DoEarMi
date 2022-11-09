49cd860f08a73625f040519cf30d8eeb620126e6
Raleigh Hansen <raleigh.sedona@gmail.com>
2022-11-09 14:10:43 -0800

    User instrument collections saved as string lists and serialized in json object

--- Assets/Scripts/LoadAudioAsInstrument.cs
+++ Assets/Scripts/LoadAudioAsInstrument.cs
@@ -4,7 +4,36 @@ using System.IO;
 using UnityEngine;
 
 public class LoadAudioAsInstrument
-{
+{   
+    // Singleton
+    private static LoadAudioAsInstrument instance = null;
+    // Thread-safety locks for singleton and files
+    private static readonly object instance_padlock = new object();
+    
+    // Directory in Resources folder containing all audio clip subfolders
+    private static string FileDirectory = "AudioClips/";
+
+    // Instance is null initializer
+    private LoadAudioAsInstrument() {}
+
+    // Entry point for DoEarMiMeta singleton
+    public static LoadAudioAsInstrument Instance()
+    {
+        // double locking
+        if (instance == null)
+        {
+            lock (instance_padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new LoadAudioAsInstrument();
+                }
+            }
+        }
+
+        return instance;
+    } 
+
     // All Instruments must be declared as elements of All_Instruments dictionary
     // key (string): instrument name
     // value (string): intermediate path to audioclips without specifying each note (must be .wav files)
@@ -21,6 +50,7 @@ public class LoadAudioAsInstrument
     {
         List<AudioClip> instrument = new List<AudioClip>();
 
+        // Ensure instrument is valid and listed
         if(!All_Instruments.TryGetValue(name, out string path))
         {
             Debug.Log("Trying to get an instrument not specified in Instruments.cs All_Instruments dictionary");
@@ -29,7 +59,7 @@ public class LoadAudioAsInstrument
         {
 
             // Load all files from specified path into objects list 
-            Object[] clips = Resources.LoadAll("AudioClips/" + path, typeof(AudioClip));
+            Object[] clips = Resources.LoadAll(FileDirectory + path, typeof(AudioClip));
 
             // Convert each object to AudioClip and add to instrument array
             for (int i = 0; i < clips.Length; i++)
@@ -40,6 +70,35 @@ public class LoadAudioAsInstrument
 
         return instrument;
     }
+
+
+    // public InstrumentPathWrapper get_instrument_paths(string name)
+    // {
+    //     InstrumentPathWrapper paths = new InstrumentPathWrapper();
+    //     paths.instrument_paths = new List<string>();
+
+    //     // Ensure instrument is valid and listed
+    //     if(!All_Instruments.TryGetValue(name, out string path))
+    //     {
+    //         Debug.Log("Trying to get an instrument not specified in Instruments.cs All_Instruments dictionary");
+    //     }
+    //     else
+    //     {
+    //         var directory = new DirectoryInfo(FileDirectory + path);
+    //         paths.instrument_paths = directory.GetFiles();
+
+    //         // // Load all files from specified path into objects list 
+    //         // Object[] clips = Resources.LoadAll(FileDirectory + path, typeof(AudioClip));
+
+    //         // // Convert each object to AudioClip and add to instrument array
+    //         // for (int i = 0; i < clips.Length; i++)
+    //         // {
+    //         //     instrument.Add((AudioClip) clips[i]);
+    //         // }
+    //     }
+
+    //     return paths;
+    // } 
     
 
     // // start method used for testing, add MonoBehaviour to script to use
