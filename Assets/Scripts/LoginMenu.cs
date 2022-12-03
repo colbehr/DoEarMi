61c5864bce1d9e37fdcf0173c466d4fa9c3d43bc
Raleigh Hansen <raleigh.sedona@gmail.com>
2022-12-02 21:11:57 -0800

    Sign in complete, create account button still does nothing

--- Assets/Scripts/LoginMenu.cs
+++ Assets/Scripts/LoginMenu.cs
@@ -11,39 +11,61 @@ public class LoginMenu : MonoBehaviour
     public GameObject practice;
     public GameObject upperNav;
     public GameObject lowerNav;
-    public GameObject email;
+    public GameObject uname;
     public GameObject password;
+    public GameObject failText;
+    // Buttons
+    public GameObject signInButton;
+    public GameObject showPassword;
+    public GameObject hidePassword;
 
     // Backend
-    private string userEmail;
-    private string userPassword; // TODO: plaintext password oof
     private DoEarMiMeta meta;
+    private string username;
+    private string userPassword; // TODO: plaintext password oof
+    private bool nameFilled;
+    private bool passwordFilled;
 
     // Start is called before the first frame update
     void Start()
     {
         practice.SetActive(false);
         upperNav.SetActive(false);
-        lowerNav.SetActive(false);
+        lowerNav.SetActive(false); 
+        failText.SetActive(false); 
 
         meta = DoEarMiMeta.Instance();
+
+        signInButton.GetComponent<Button>().interactable = false;
+        this.nameFilled = false;
+        this.passwordFilled = false;
+
+        showPassword.SetActive(false);
+        hidePassword.SetActive(true);
     }
 
-    public void set_user_email()
+    public void set_user_name()
     {
-        // Debug.Log("email set");
-        this.userEmail = email.GetComponent<TMP_InputField>().text;
-        // Debug.Log(userEmail);
+        // Debug.Log("uname set");
+        this.username = uname.GetComponent<TMP_InputField>().text;
+        this.nameFilled = true;
+
+        failText.SetActive(false); 
+        attempt_enable_signin();
     }
     public void set_user_password()
     {
         // Debug.Log("password set");
         this.userPassword = password.GetComponent<TMP_InputField>().text;
+        this.passwordFilled = true;
+
+        failText.SetActive(false); 
+        attempt_enable_signin();
     }
 
     public void sign_in()
     {
-        User user = meta.find_user(userEmail);
+        User user = meta.find_user(username);
 
         if (user != null && user.get_password() == userPassword)
         {
@@ -61,9 +83,7 @@ public class LoginMenu : MonoBehaviour
         }
         else
         {
-            Debug.Log("no user matching name");
-
-            // TODO: bad sign in, reprompt
+            failText.SetActive(true); 
         }
     }
 
@@ -73,12 +93,39 @@ public class LoginMenu : MonoBehaviour
         User user = meta.find_user("ShrimpAce");
         meta.set_curr_user(user);
 
-        Debug.Log(user.get_uID());
+        // Debug.Log(user.get_uID());
 
         login.SetActive(false);
         practice.SetActive(true);
         upperNav.SetActive(true);
         lowerNav.SetActive(true);
+    }
+
+
+    // Makes sign in button clickable if input fields are populated
+    public void attempt_enable_signin()
+    {
+        if (this.nameFilled && this.passwordFilled)
+        {
+            this.signInButton.GetComponent<Button>().interactable = true;
+        }
+    }
+
+
+    public void hide_password()
+    {
+        password.GetComponent<TMP_InputField>().contentType = TMP_InputField.ContentType.Password;
+        showPassword.SetActive(false);
+        hidePassword.SetActive(true);
+        password.GetComponent<TMP_InputField>().ForceLabelUpdate();
+    }
+
+    public void show_password()
+    {
+        password.GetComponent<TMP_InputField>().contentType = TMP_InputField.ContentType.Standard;
+        showPassword.SetActive(true);
+        hidePassword.SetActive(false);
+        password.GetComponent<TMP_InputField>().ForceLabelUpdate();
     }
 
     // TODO: for now, can probably simulate log in by creating a new user... 
